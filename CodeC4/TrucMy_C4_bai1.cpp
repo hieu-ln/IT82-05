792056b9a174b6e9cfa1821bca279d56b2ae5fd9
Trucmy102 <1854050059my@ou.edu.vn>
2019-07-31 10:28:38 +0700

    nop bai chuong 4

--- CodeC4/TrucMy_C4_bai1.cpp
+++ CodeC4/TrucMy_C4_bai1.cpp
@@ -1,242 +1,261 @@
-//Bai tap 1 - Chuong 4
+// chuoong 4 bai 1 
 #include <iostream>
-#include <stdio.h>
+#include<stdio.h>
 using namespace std;
+
 #define COUNT 10
-//Cau 1.1 Khai bao cau truc cay nhi phan tim kiem
-struct Node
+
+// cau 1.1 khai bao cua truc cay NPTK
+struct Node 
 {
-	int info;
+	int info ;
 	Node *left;
 	Node *right;
 };
 Node *root;
-//Cau 1.2 Khoi tao cay rong
-void Init()
+
+// cau 1.2: khoi tao cay rong
+void init()
 {
 	root = NULL;
 }
-//Cau 1.3 Them 1 phan tu vao cay
-void InsertNode(Node *&p, int x)
+
+// cau 1.3: Them mot phan tu su dung de quy
+int Insert_Node_Recursive(int x, Node *q)
 {
-	if(p==NULL)
+	if(q-> info == x)
+		return 0;
+	else if(q -> info > x)
 	{
-		p=new Node;
-		p->info=x;
-		p->left=NULL;
-		p->right=NULL;
+		if(q-> left == NULL)
+		{
+			Node *p = new Node;
+			p->info=x;
+			p->left = NULL;
+			p->right = NULL;
+			p->left = p;
+			return 1;
+		}
+		else
+			return Insert_Node_Recursive(x, q->left);
 	}
 	else
 	{
-		if(p->info==x)
-			return;
-		else if(p->info >x)
-			return InsertNode(p->left, x);
+		if(q->right ==NULL)
+		{
+			Node *p = new Node;
+			p->info = x;
+			p->left = NULL;
+			p->right =  NULL;
+			p->right = p;
+			return 1;
+		}
 		else
-			return InsertNode(p->right, x);
+			return Insert_Node_Recursive(x, q->right);
 	}
 }
-//int Insert_Node_Recursive(int x, Node *q)
-//{
-//	if(q->info == x)
-//		return 0;
-//	else if(q->info > x)
-//	{
-//		if(q->left==NULL)
-//		{
-//			Node *p = new Node;
-//			p->info = x;
-//			p->left = NULL;
-//			p->right = NULL;
-//			q->left = p;
-//			return 1;
-//		}
-//		else
-//			return Insert_Node_Recursive(x, q->right);
-//	}
-//	else
-//	{
-//		if(q->right == NULL)
-//		{
-//			Node *p = new Node;
-//			p->info = x;
-//			p->left = NULL;
-//			p->right = NULL;
-//			q->right = p;
-//			return 1;
-//		}
-//	}
-//}
-//Cau 1.4 Tim 1 phan tu trong cay
-Node *search(Node *p, int x)
+
+// cau 1.3: su dung dua chi con tro
+void InsertNode(Node *&p, int x)
 {
-	if(p!= NULL)
+	if(p == NULL)
 	{
-		if(p->info==x)
+		p=new Node;
+		p->info = x;
+		p->left = NULL;
+		p->right =NULL;
+	}
+	else
+	{
+		if(p->info == x)
+			return ;
+		else if (p->info >x)
+			return InsertNode(p->left,x);
+		else
+			return InsertNode(p->right,x);
+	}
+}
+
+
+
+// cau 1.4: tim mot phan tu dung de quy
+Node *Search(Node *p, int x)
+{
+	if(p != NULL)
+	{
+		if(p->info == x)
 			return p;
 		else
-			if(x>p->info)
-				return search(p->right, x);
+			if(x > p->info)
+				return Search(p->right, x);
 			else
-				return search(p->left, x);
+				return Search(p->left, x);
 	}
 	return NULL;
 }
-//Cau 1.5 Xoa 1 nut trong cay
-void searchStandFor(Node *&p, Node*&q)
+
+// cau 1.5: xoa nut trong cay
+void SearchStandFor(Node *&p, Node *&q)
 {
-	if(p->left == NULL)
+	if(q->left == NULL)
 	{
 		p->info = q->info;
-		p=q;
-		q=q->right;
+		p =q;
+		q = q->right;
 	}
 	else
-		searchStandFor(p, q->left);
+		SearchStandFor(p, q->left);
 }
+
 int Delete(Node *&T, int x)
 {
 	if(T==NULL)
 		return 0;
-	if(T->info==x)
+	if(T->info == x)
 	{
-		Node *p = T;
-		if(T->left == NULL)
+		Node *p= T;
+		if(T->left==NULL)
 			T=T->right;
-		else if(T->right==NULL)
+		else if(T->right ==NULL)
 			T=T->left;
 		else
-			searchStandFor(p, T->right);
+			SearchStandFor(p, T->right);
 		delete p;
 		return 1;
 	}
-	if(T->info<x)
-		return Delete(T->right, x);
-	if(T->info>x)
-		return Delete(T->left, x);
+	if(T->info<x) return Delete (T->right, x);
+	if(T->info>x) return Delete(T->left, x);
+
 }
-//Cau 1.6 Duyet cay theo thu tu NLR
-void duyetNLR(Node *p)
+
+// cau 1.6: Duyet cay theo LNR
+void DuyetLNR(Node *p)
 {
-	if(p!=NULL)
+	if(p!= NULL)
 	{
+		DuyetLNR(p->left);
 		cout << p->info << " ";
-		duyetNLR(p->left);
-		duyetNLR(p->right);
+		DuyetLNR(p->right);
 	}
 }
-//Cau 1.7 Duyet cay theo thu tu LNR
-void duyetLNR(Node *p)
+
+//cau 1.7: Duyet cay theo NLR 
+void DuyetNLR(Node *p)
 {
-	if(p!=NULL)
+	if(p!= NULL)
 	{
-		duyetLNR(p->left);
 		cout << p->info << " ";
-		duyetLNR(p->right);
+		DuyetNLR(p->left);
+		DuyetNLR(p->right);
 	}
 }
-//Cau 1.8 Duyet cay theo thu tu LRN
-void duyetLRN(Node *p)
+
+//  cau 1.8: Duyet cay theo LRN
+void DuyetLRN(Node *p)
 {
-	if(p!=NULL)
+	if(p!= NULL)
 	{
-		duyetLRN(p->left);
-		duyetLRN(p->right);
+		DuyetLNR(p->left);
+		DuyetLNR(p->right);
 		cout << p->info << " ";
 	}
 }
-//XUAT
+
 void print2DUtil(Node *p, int space)
 {
 	if(p==NULL)
-		return;
+		return ;
 	space += COUNT;
 	print2DUtil(p->right, space);
 	cout << endl;
-	for(int i = COUNT; i<space; i++)
+	for(int i = COUNT; i < space; i++)
 		cout << " ";
 	cout << p->info << "\n";
 	print2DUtil(p->left, space);
 }
+
 void Process_Tree()
 {
 	print2DUtil(root, 0);
 }
+
 int main()
 {
 	int choice = 0;
 	int x, i;
 	Node *p;
 	system("cls");
-	cout << "----------BAI TAP 1 - CHUONG 4 - CAY NPTK----------\n";
-	cout << "1. Khoi tao CAY NPTK rong\n";
-	cout << "2. Them phan tu vao CAY NPTK\n";
-	cout << "3. Tim phan tu co gia tri x trong CAY NPTK\n";
-	cout << "4. Xoa phan tu co gia tri x trong CAY NPTK\n";
-	cout << "5. Duyet CAY NPTK theo thu tu NLR\n";
-	cout << "6. Duyet CAY NPTK theo thu tu LNR\n";
-	cout << "7. Duyet CAY NPTK theo thu tu LRN\n";
-	cout << "8. Xuat CAY NPTK\n";
-	cout << "9. Thoat\n";
+	cout << "-------- BAI TAP 1 , CHUONG 4 , CAY NPTK--------"<< endl;
+	cout << "1. Khoi tao cay NPTK"<< endl;
+	cout << "2. Them phan tu vao cay NPTK" << endl;
+	cout << "3. Tim phan tu co gia tri x trong cay NPTK"<< endl;
+	cout << "4. Xoa phan tu co gia tri x trong cay NPTK"<<endl;
+	cout << "5. Duyet cay NPTK theo LNR"<< endl;
+	cout << "6. Duyet cay NPTL theo NLR"<< endl;
+	cout << "7. Duyet cay NPTK theo LRN"<< endl;
+	cout << "8. Xuat cay NPTK"<<endl;
+	cout << "9. Thoat "<< endl;
 	do{
-		cout << "\nVui long chon so de thuc hien: ";
+		cout << "\nVui long chon so de thuc hien:";
 		cin >> choice;
 		switch(choice)
 		{
-		case 1:
-			Init();
-			cout << "Ban vua khoi tao CAY NPTK thanh cong!\n";
-			break;
-		case 2:
-			cout << "Vui long nhap gia tri x can them : ";
-			cin >> x;
-			InsertNode(root, x);
-			cout << "CAY NPTK sau khi da them la: ";
-			Process_Tree();
-			break;
-		case 3:
-			cout << "Vui long nhap gia tri x can tim: ";
-			cin >> x;
-			p = search(root, x);
-			if(p!=NULL)
-				cout << "Tim thay x = " << x << " trong CAY NPTK\n";
-			break;
-		case 4:
-			cout << "Vui long nhap gia tri x can xoa: ";
-			cin >> x;
-			i = Delete(root, x);
-			if(i == 0)
-				cout << "Khong tim thay x = " << x << " de xoa!\n";
-			else
-			{
-				cout << "Da xoa thanh cong phan tu x = " << x << " trong CAY NPTK!\n";
-				cout << "CAY NPTK sau khi da xoa la: ";
+			case 1:
+				init();
+				cout << "Ban vui long khoi tao cay NPTK thanh cong!\n";
+				break;
+			case 2: 
+				cout << "Vui long nhap gia tri x can them";
+				cin >> x;
+				InsertNode(root,x);
+				cout << "Cay NPTK sau khi them la: ";
 				Process_Tree();
-			}
-			break;
-		case 5:
-			cout << "CAY NPTK duyet theo NLR la: ";
-			duyetNLR(root);
-			break;
-		case 6:
-			cout << "CAY NPTK duyet theo LNR la: ";
-			duyetLNR(root);
-			break;
-		case 7:
-			cout << "CAY NPTK duyet theo LRN la: ";
-			duyetLRN(root);
-			break;
-		case 8:
-			cout << "CAY NPTK nhu sau: ";
-			Process_Tree();
-			break;
-		case 9:
-			cout << "Good bye!!\n";
-			break;
-		default:
-			break;
+				break;
+			case 3:
+				cout << "Vui long nhap gia tri x can tim";
+				cin >> x;
+				p= Search(root, x);
+				if(p!=NULL)
+					cout << "Tim thay x="<<x<< "trong cau NPTK"<<endl;
+				break;
+			case 4:
+				cout << "Vui long nhap gia tri x can xoa";
+				cin>>x;
+				i = Delete(root,x);
+				if(i == 0)
+					cout << "Khong tim thay x"<<x<<"trong cay NPTK"<< endl;
+				else
+				{
+					cout << "Da xoa thanh cong phan tu x="<<x<<"trong cay NPTK"<<endl;
+					cout << "Cay NPTK sau khi xoa la:";
+					Process_Tree();
+				}
+				break;
+			case 5:
+				cout << "Cay NPTK duyet theo LNR la:";
+				DuyetLNR(root);
+				break;
+			case 6:
+				cout << "Cay NPTK duyet theo NLR la: ";
+				DuyetNLR(root);
+				break;
+			case 7:
+				cout << "Cay NPTK duyet theo LRN la:";
+				DuyetLRN(root);
+				break;
+			case 8:
+				cout << "Cay NPTK nhu sau:";
+				Process_Tree();
+				break;
+			case 9:
+				cout << "goobye----"<<endl;
+				break;
+			default:
+				break;
+
 		}
 	}while(choice != 9);
 	system("pause");
 	return 0;
+
 }
